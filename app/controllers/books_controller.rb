15bbd97a35e0779ad40279210f492ac6ad5ca4fd
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2023-08-16 10:44:32 +0000

    [Finish]3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,56 +1,66 @@
 class BooksController < ApplicationController
   
+  def new
+    
+   
   
-  
-  
+  end
+
+
   def create
     @book = Book.new(book_params)
-    
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
-    else
-      flash.now[:notice] = "投稿に失敗しました。"
-      render :new
+    else 
+      @books = Book.all
+      render :index
     end
-    
+
   end
-  
+
   def index
     @book = Book.new
-    @books = Book.all  
-  end 
-  
+    @books = Book.all
+  end
+
 
   def show
-    @book = Book.find(params[:id]) 
+   
+    @book = Book.find(params[:id])
+    
   end
-  
+
 
   def edit
+  
     @book = Book.find(params[:id])
-     
-  end
-  
-  
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+   
   end  
-  
-  
+
+  def update
+   
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else 
+      render :edit
+    end  
+  end
+
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
-  end  
-  
-  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+
    private
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-end 
+
+end
